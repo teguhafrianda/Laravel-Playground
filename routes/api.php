a11d1de1eb62a14e880d8480604a27320e4d0eb7
samsul_apexa <apexasamsulmarif@gmail.com>
2025-03-02 09:01:26 +0800

    soal2

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,33 +8,26 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/try/{a}/{b}/{c}', function ($a, $b, $c) {
+const SUCCESS_UNIT_TESTING = 80;
+
+$response = function (): void {};
+
+Route::post('/project/create/cortex', function (Request $request) {
+
     /**
-     * 
-     * query params pake get (format: http://127.0.0.1:8000/api/try?a=1&b=2)
-     * body params pake post (format: json,form data dst)
-     * route params(path) all request methods (formatnya: http://127.0.0.1:8000/api/try/1/1)
-     * 
-     * request param: query,body,route(path)
-     * 
-     * {a}/{b} <- model binding untuk menyiapkan params input yang nanti nya akan di proses secara function
+     * create request BODY
      */
-
-    // $a = $request->query('a'); //req query artinya adalah requestnya hanya bisa melalui query params
-    // $b = $request->query('b');
-
-    // $a = $request->post('a'); //req body artinya adalah requestnya hanya bisa melalui body params
-    // $b = $request->post('b');
-
-    $c = $a + $b * $c;
-    if (empty($a) || empty($b)) {
-        return response()->json(['message' => 'input masih kosong']);
+    $unit_testing = [];
+    $request = $request->post();
+    foreach ($request as $key => $value) {
+        $value = array(
+            'name' => $value['name'],
+            'stack' => $value['stack'],
+            'skor' => array_sum($value['unit_testing_skor']),
+            'status' => array_sum($value['unit_testing_skor']) >= SUCCESS_UNIT_TESTING ? 'success' : 'failed'
+        );
+        array_push($unit_testing, $value);
     }
 
-    if ($c < 3) {
-        $h = 'lebih kecil dari 3';
-    } else {
-        $h = 'lebih besar 3';
-    }
-    return response()->json(['message' => $h]);
+    return response()->json($unit_testing);
 });
